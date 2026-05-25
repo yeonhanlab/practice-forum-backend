fd350d15f3050f4623ec7a3bb82392fd1e57284a
yeonhan <yeonhanlab@gmail.com>
2026-05-25 19:54:27 +0900

    validate middleware 추가 (경로수정)

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import userController from "../controllers/userController.ts";
 import { validate } from "../middlewares/validate.ts";
-import { createUserSchema } from "../../schemas/user/createUser.ts";
+import { createUserSchema } from "../schemas/user/createUser.ts";
 
 const router = Router();
 
--- src/schemas/user/createUser.ts
+++ src/schemas/user/createUser.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { GenderType } from "../../src/generated/prisma/enums.ts";
+import { GenderType } from "../../generated/prisma/enums.ts";
 
 // 검증을 할 명세. 아직 기능을 걸지는 않은 상태
 // zod를 통해 검증할 Input 값에 대한 객체모양 생성
